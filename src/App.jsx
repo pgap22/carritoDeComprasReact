47da4e43954c3fc8e003e40d8e23d51b9549c280
pgap22 <78711175+pgap22@users.noreply.github.com>
2023-04-10 20:16:27 -0600

    Conditional Rendering

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,10 @@ import Items from "./components/Items";
 
 const App = () => {
   const [carrito, modificarCarrito] = useState([]);
+
   return (
     <div>
+        
       <h1 className="font-bold text-3xl text-center p-6">Tienda E-Commerce</h1>
 
       {/* Primero aca vamos a poner nuestros productos */}
--- src/components/CarritoCompras.jsx
+++ src/components/CarritoCompras.jsx
@@ -1,22 +1,42 @@
 import ItemCarrito from "./ItemCarrito";
 
 const CarritoCompras = ({ carrito, modificarCarrito }) => {
+  const precioTotal = carrito.reduce((total, item) => total + item.precio, 0);
+
   return (
     <div className="absolute max-h-[400px] overflow-auto  bottom-0 right-0 border border-gray-200  shadow-lg">
       <div className="sticky top-0 bg-white p-4">
         <h2 className="text-4xl font-bold">Mi Carrito De Compras</h2>
-        <p className="font-bold">Precio Total: ???</p>
+        <p className="font-bold">Precio Total: {precioTotal}</p>
       </div>
       <div className="p-4 flex flex-col gap-6">
-        {carrito.map((item) => (
-          <ItemCarrito
-            key={item.id}
-            {...item}
-            id={item.id}
-            carrito={carrito}
-            modificarCarrito={modificarCarrito}
-          />
-        ))}
+        {/* {carrito.length > 0 && carrito.map((item) => (
+            <ItemCarrito
+              key={item.id}
+              {...item}
+              id={item.id}
+              carrito={carrito}
+              modificarCarrito={modificarCarrito}
+            />
+          ))}
+
+          {
+            carrito.length < 0 && 'No tienes items en el carrito :('
+          } */}
+
+          {
+            carrito.length > 0
+            ? carrito.map((item) => (
+                <ItemCarrito
+                  key={item.id}
+                  {...item}
+                  id={item.id}
+                  carrito={carrito}
+                  modificarCarrito={modificarCarrito}
+                />
+              ))
+              : <p>No tienes items en el carrito</p>
+          }
       </div>
     </div>
   );
